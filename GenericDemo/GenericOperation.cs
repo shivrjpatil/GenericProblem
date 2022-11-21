17c048bf747b2ee4e680225f86dde09a9aa45b0a
shivrjpatil <shivrajp972@gmail.com>
2022-11-21 21:54:24 +0530

    [shiv]add:generic problem

---
+++ GenericDemo/GenericOperation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    internal class GenericOperation
+    {
+        int[] intArr = { 1, 2, 3, 4, 5, 6, 7 };
+        double[] doubleArr = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+        char[] charArr = { 'a', 'b', 'c', 'd', 'e' };
+        public void ToPrintArray<T>(T[] intArr)
+        {
+            foreach (var data in intArr)
+            {
+                Console.WriteLine(data);
+            }
+        }
+        public void ToPrintArray(double[] intArr)
+        {
+            foreach (var data in intArr)
+            {
+                Console.WriteLine(data);
+            }
+        }
+        public void ToPrintArray(char[] intArr)
+        {
+            foreach (var data in intArr)
+            {
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
+    
+
+        
---
+++ GenericDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace GenericDemo
+{
+    class progarm
+    {
+        static void Main(string[] args)
+        {
+           int[] intArr = { 1, 2, 3, 4, 5, 6, 7 };
+            double[] doubleArr = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+            char[] charArr = { 'a', 'b', 'c', 'd', 'e' };
+            GenericOperation generic = new GenericOperation();
+            generic.ToPrintArray(intArr);
+            generic.ToPrintArray(doubleArr);
+            generic.ToPrintArray(charArr);
+        }
+    }
+}
